72bc70478cf5606610dc8b4409810297657cabd2
Alexander Shevtsov <alecsandr.unit@gmail.com>
2025-10-24 01:36:51 +0300

    HW_2

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,6 +13,17 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private String[] songLyrics = {
+            "Куплет 1: Наш перший рядок пісні.",
+            "Приспів: Наш приспів, він повторюється.",
+            "Куплет 2: Другий куплет, нові слова.",
+            "Приспів: Наш приспів, він повторюється.",
+            "Куплет 3: Фінальний куплет."
+    };
+
+    private int currentLineIndex = 0;
+    Button songButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,11 +35,29 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        songButton = findViewById(R.id.songButton);
+        songButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showNextLyric();
+            }
+        });
     }
 
-    public void increaseWidth(View view) {
-        ViewGroup.LayoutParams params = view.getLayoutParams();
-        params.width = params.width + 20;
-        view.setLayoutParams(params);
+    private void showNextLyric() {
+        if (songLyrics == null || songLyrics.length == 0) {
+            return;
+        }
+
+        String currentLine = songLyrics[currentLineIndex];
+        Toast.makeText(this, currentLine, Toast.LENGTH_LONG).show();
+
+        currentLineIndex++;
+        if (currentLineIndex >= songLyrics.length) {
+            currentLineIndex = 0;
+        }
     }
+
+
+
 }
